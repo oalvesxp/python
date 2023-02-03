56406e44a48e2904b6a73d97818001975ba59f9a
Osvaldo alves <oalvesxp@gmail.com>
2023-02-03 20:19:34 -0300

    first scripts in python

---
+++ lessons/aula02/calc.py
@@ -0,0 +1,15 @@
+# @file: calc.py
+# @author: Osvaldo Alves
+# @date: 03/02/2023
+# @description: learning to do calculations with variables.
+# @more: https://pythonacademy.com.br/blog/operadores-aritmeticos-e-logicos-em-python
+
+a = 16 
+b = 2
+c = 5
+
+print(a, '+', b, '=', a + b)    # sum 
+print(a, '-', c, '=', a - c)    # subtraction
+print(b, 'x', c, '=', b * c)    # multiplication
+print(a, '/', b, '=', a / b)    # division
+print(c, '^', b, '=', c ** b)   # exponetial
---
+++ lessons/aula02/input-output.py
@@ -0,0 +1,13 @@
+# @file: input-output.py
+# @author: Osvaldo Alves
+# @date: 03/02/2023
+# @description: learning to input values in variables with function.
+
+name = input('Insert your name: ')
+age = input('Insert your age: ')
+height = input('Insert your height: ')
+
+print('\n')
+print(name, type(name))
+print(age, type(age))
+print(height, type(height), '\n')
