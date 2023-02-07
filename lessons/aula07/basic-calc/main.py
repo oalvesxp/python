a6ea81faa27d957f2b7d327ab8a591ed3f1daf5c
Osvaldo alves <oalvesxp@gmail.com>
2023-02-07 14:35:11 -0300

    exercise from aula07 - creating a calculator

---
+++ lessons/aula07/basic-calc/main.py
@@ -0,0 +1,24 @@
+# @file: main.py
+# @includes: module.py
+# @author: Osvaldo Alves
+# @date: 07/02/2023
+# @description: Exercise of calculator with methods, functions and imports.
+
+# imports
+import src.soma
+import src.sub
+import src.mult
+import src.div
+
+# maths operations
+soma = src.soma.soma(23, 56)
+print(f'Resultado da soma: {soma}')
+
+sub = src.sub.subtrai(58, 23)
+print(f'Resultado da subtração: {sub}')
+
+mult = src.mult.multiplica(9, 7)
+print(f'Resultado da multiplicação: {mult}')
+
+div = src.div.divide(32, 4)
+print(f'Resultado da divisão: {div}')
---
+++ lessons/aula07/basic-calc/src/sub.py
@@ -0,0 +1,8 @@
+# @file: sub.py
+# @author: Osvaldo Alves
+# @date: 07/02/2023
+# @description: Exercise of calculator with methods and functions.
+
+def subtrai(a, b):
+    value = a - b
+    return value
