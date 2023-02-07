6e14555dbdffe4aa78df235bc3294672651fe4cd
Osvaldo alves <oalvesxp@gmail.com>
2023-02-07 16:39:17 -0300

    pushing fresh

--- lessons/aula08/main.py
+++ lessons/aula08/main.py
@@ -2,7 +2,7 @@
 # @author: Osvaldo Alves
 # @date: 07/02/2023
 # @description: Exercise of calculator with methods, functions and imports.
-# @use: ./main.py [args[1]] [args[2]] [args[3]]
+# @use: ./main.py [args[1]].. [args[2]].. [args[3]]..
 # @exemple: ./main.py multiplicar 8 9
 
 #imports
